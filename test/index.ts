d80a4054cefb829a8c6f597bee83f59cf6e5a5d5
Danila Pisarev <jobdan@bk.ru>
2022-03-14 17:16:28 +0300

    Added preset for tests

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,25 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import {
+  ERC721Token,
+  ERC721Token__factory,
+  Marketplace,
+  Marketplace__factory,
+} from "../typechain";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Marketplace", function () {
+  let erc721Token: ERC721Token;
+  let marketplace: Marketplace;
+  beforeEach(async () => {
+    const marketplaceFactory: Marketplace__factory =
+      await ethers.getContractFactory("Marketplace");
+    marketplace = await marketplaceFactory.deploy();
+    await marketplace.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    const erc721Factory: ERC721Token__factory = await ethers.getContractFactory(
+      "ERC721Token"
+    );
+    erc721Token = await erc721Factory.deploy("TOKEN", "T", marketplace.address);
+    await erc721Token.deployed();
   });
 });
